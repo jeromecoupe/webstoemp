cc01182f01607fd4ca70b427b6cc17f7bccc2f81
Jérôme Coupé <jercoupe@gmail.com>
2015-02-25 18:30:49 +0100

    JS improvements
    - integrated cut the mustard test
    
    Signed-off-by: Jérôme Coupé <jercoupe@gmail.com>

--- js/webstoemp.js
+++ js/webstoemp.js
@@ -85,65 +85,77 @@
 
 });
 
-if ('querySelector' in document && 'addEventListener' in window) {
+var myNavigation = (function () {
 
-	var myNavigation = (function () {
+	'use strict';
 
-		'use strict';
+	//create vars
+	var menuLink,
+			menu,
+			menuState;
 
-		//set vars
-		var menuLink = document.querySelector('.js-mainnav-compact > a'),
-				menu = document.querySelector('.js-mainnav'),
-				menuState = 'closed';
+	//check dependencies
+	var checkDependencies = function() {
+		return ('querySelector' in document && 'addEventListener' in window);
+	};
 
-		// initiate navigation
-		var initNav = function() {
+	// initiate navigation
+	var initNav = function() {
 
+		if(checkDependencies())
+		{
+			// set vars
+			menuLink = document.querySelector('.js-mainnav-compact > a');
+			menu = document.querySelector('.js-mainnav');
+			menuState = 'closed';
+
+			//hide menu
 			apollo.addClass(menu, 'js-is-hidden');
+
+			//bin actions
 			bindActions();
+		}
 
-		};
+	};
 
-		//bind navigation actions
-		var bindActions = function() {
+	//bind navigation actions
+	var bindActions = function() {
 
-			menuLink.addEventListener('click', function(e) {
+		menuLink.addEventListener('click', function(e) {
 
-				toggleMenu(menuState);
-				e.preventDefault();
-				//console.log(menuState);
+			toggleMenu(menuState);
+			e.preventDefault();
+			//console.log(menuState);
 
-			}, false);
+		}, false);
 
-		};
+	};
 
-		//toggle menu
-		var toggleMenu = function(state) {
+	//toggle menu
+	var toggleMenu = function(state) {
 
-			//swap classes
-			if (state === 'closed' ){
-				apollo.removeClass(menu, 'js-is-hidden');
-				apollo.addClass(menu, 'js-is-visible');
-			} else {
-				apollo.removeClass(menu, 'js-is-visible');
-				apollo.addClass(menu, 'js-is-hidden');
-			}
+		//swap classes
+		if (state === 'closed' ){
+			apollo.removeClass(menu, 'js-is-hidden');
+			apollo.addClass(menu, 'js-is-visible');
+		} else {
+			apollo.removeClass(menu, 'js-is-visible');
+			apollo.addClass(menu, 'js-is-hidden');
+		}
 
-			// toggle menu state
-			menuState = (state === 'closed') ? 'open' : 'closed';
+		// toggle menu state
+		menuState = (state === 'closed') ? 'open' : 'closed';
 
-		};
+	};
 
-		//make init publicly accessible
-		return {
-			initNav:initNav
-		};
+	//make init publicly accessible
+	return {
+		initNav:initNav
+	};
 
-	}());
+}());
 
-	myNavigation.initNav();
-
-}
+myNavigation.initNav();
 (function(){
 
 	'use strict';
