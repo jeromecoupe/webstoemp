b6a10ce3ef22c368ab7cf3942cc7c3a99fca4ff9
jérôme coupé <jercoupe@gmail.com>
2020-04-07 11:19:44 +0200

    Prettier formatting

--- gulp-tasks/eleventy.js
+++ gulp-tasks/eleventy.js
@@ -3,10 +3,12 @@ const cp = require("child_process");
 
 // Eleventy
 function eleventyBuild() {
-  return cp.spawn("npx", ["eleventy", "--quiet"], { stdio: "inherit" });
+  return cp.spawn("npx", ["eleventy", "--quiet"], {
+    stdio: "inherit",
+  });
 }
 
 // exports
 module.exports = {
-  build: eleventyBuild
+  build: eleventyBuild,
 };
