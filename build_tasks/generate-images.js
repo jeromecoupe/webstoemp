5730c1ad300dcb0f9fe7c4f98af171323e150e5d
Jérôme Coupé <jercoupe@gmail.com>
2023-05-14 17:26:14 +0200

    simpler generate images script

--- build_tasks/generate-images.js
+++ build_tasks/generate-images.js
@@ -2,6 +2,11 @@ const fs = require("fs");
 const path = require("path");
 const { globSync } = require("glob");
 const sharp = require("sharp");
+
+const config = {
+  allowedFormats: ["jpg", "jpeg", "webp", "avif", "png", "gif"],
+};
+
 const transforms = [
   {
     // 1024x576 thumbnails for blogposts
@@ -11,14 +16,14 @@ const transforms = [
     options: {
       width: 1024,
       height: 576,
-      fit: "cover",
+      pouet: "cover",
     },
   },
   {
     // 600x600 thumbnails for blogposts
     src: "./src/assets/img/blogposts/",
     dist: "./dist/assets/img/blogposts/600x600/",
-    formats: ["jpg", "webp"],
+    formats: ["jpg"],
     options: {
       width: 600,
       height: 600,
@@ -29,7 +34,7 @@ const transforms = [
     // 800x600 thumbnails for projects
     src: "./src/assets/img/projects/",
     dist: "./dist/assets/img/projects/800x600/",
-    formats: ["jpg", "webp"],
+    formats: ["jpg"],
     options: {
       width: 800,
       height: 600,
@@ -52,16 +57,33 @@ async function init() {
     let formats = transform.formats;
     let options = transform.options;
 
+    // check formats is array
+    if (!Array.isArray(formats)) {
+      throw new Error(`"formats" in transforms should be an array`);
+    }
+
+    // check formats are of allowed types
+    formats.forEach((el) => {
+      if (!config.allowedFormats.includes(el)) {
+        throw new Error(
+          `Unknown format: "${el}". Allowed formats are: ${config.allowedFormats.toString()}`
+        );
+      }
+    });
+
+    // Get image files in input directory
+    let imagesGlob = path.join(
+      inputDir,
+      `*.{${config.allowedFormats.toString()}}`
+    );
+    let imagesFiles = globSync(imagesGlob);
+
     // Create output dir if does not exists
     if (!fs.existsSync(outputDir)) {
       fs.mkdirSync(outputDir, { recursive: true });
     }
 
-    // Get image files in input directory
-    let imagesGlob = path.join(inputDir, "*.{jpg,jpeg,webp,png}");
-    let imagesFiles = globSync(imagesGlob);
-
-    // Resize and save each image to the output directory
+    // loop through all images and create Sharp promises
     imagesFiles.forEach((file) => {
       // Create resized images for each specified formats
       formats.forEach((format) => {
@@ -74,15 +96,10 @@ async function init() {
         // bail out if image output path exists
         if (fs.existsSync(outputPath)) return;
 
-        // generate sharp promises
+        // create sharp promises
         try {
           // resize promise
-          let sharpPromise = sharp(file)
-            .resize(options)
-            .toFile(outputPath)
-            .catch((err) => {
-              throw new Error(err);
-            });
+          let sharpPromise = sharp(file).resize(options).toFile(outputPath);
 
           // push promise to array
           sharpPromises.push(sharpPromise);
@@ -96,7 +113,7 @@ async function init() {
   // resolve all promises in parallel
   try {
     await Promise.all(sharpPromises);
-    console.log(`${sharpPromises.length} images processed`);
+    console.log(`${sharpPromises.length} resized images generated`);
   } catch (err) {
     throw new Error(err);
   }
